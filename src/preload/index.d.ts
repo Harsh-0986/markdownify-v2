383336818e491d9be97f4eeb49d9cf5063141641
Harsh-0986 <harsh2102.dev@gmail.com>
2024-01-13 15:55:53 +0530

    Fetching files from fs

--- src/preload/index.d.ts
+++ src/preload/index.d.ts
@@ -1,10 +1,11 @@
-import { GetNotes } from '@shared/types'
+import { GetNotes, ReadNote } from '@shared/types'
 
 declare global {
   interface Window {
     context: {
       locale: string
       getNotes: GetNotes
+      readNote: ReadNote
     }
   }
 }
--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,4 +1,4 @@
-import { GetNotes } from '@shared/types'
+import { GetNotes, ReadNote } from '@shared/types'
 import { contextBridge, ipcRenderer } from 'electron'
 
 if (!process.contextIsolated) {
@@ -8,7 +8,8 @@ if (!process.contextIsolated) {
 try {
   contextBridge.exposeInMainWorld('context', {
     locale: navigator.language,
-    getNotes: (...args: Parameters<GetNotes>) => ipcRenderer.invoke('getNotes', ...args)
+    getNotes: (...args: Parameters<GetNotes>) => ipcRenderer.invoke('getNotes', ...args),
+    readNote: (...args: Parameters<ReadNote>) => ipcRenderer.invoke('readNote', ...args)
   })
 } catch (error) {
   console.error(error)
